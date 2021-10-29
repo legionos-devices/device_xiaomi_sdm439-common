fd20e331f39444f31a46d985790ba6bb0b4cf9f2
Chuck439 <firmankediri123@gmail.com>
2021-10-29 09:13:18 +0000

    sdm439-common: Build in tree PowerHAL
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -269,7 +269,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.sdm439 \
     android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.2.vendor
 
